ec05a60e52a9749623dc698079b074d1e5e86458
Gilson Alencar <gilson.alencar94@gmail.com>
2024-04-20 17:25:34 -0300

    feat: add Pack entity for managing product packs

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Pack } from './packs.entity';
 
 @Entity('products')
 export class Product {
@@ -13,4 +14,7 @@ export class Product {
 
   @Column({ type: 'decimal', precision: 9, scale: 2, nullable: false })
   sales_price: number;
+
+  @OneToMany(() => Pack, (pack) => pack.product)
+  packs: Pack[];
 }
